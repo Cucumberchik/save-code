bf5d12a62e72794d237c7380688553b138f8265d
Cucumberchik <durovalexei93@gmail.com>
2024-05-27 11:43:17 +0600

    added languages and add docs page

--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,35 @@
 
-export const LANGUAGE_VERSIONS:any = {
-    javascript: "18.15.0",
-    typescript: "5.0.3",
-    python: "3.10.0",
-    java: "15.0.2",
-    csharp: "6.12.0",
-    php: "8.2.3"
-  };
-  
-export const CODE_SNIPPETS:any = {
-    Javascript: "",
-    Typescript: "",
-    Python: "",
-    Java: "",
-    Csharp: "",
-    PHP: ""
+export const LANGUAGE_VERSIONS: any = {
+  javascript: "18.15.0",
+  typescript: "5.0.3",
+  python: "3.10.0",
+  java: "17.0.0", 
+  csharp: "10.0.0", 
+  php: "8.2.3",
+  cpp: "10.2.0",
+  ruby: "3.0.1", 
+  swift: "5.3.3", 
+  rust: "1.68.2", 
+  dart: "2.19.6",
+  go: "1.16.2",
+  kotlin: "1.8.20",
 };
   
+export const CODE_SNIPPETS:any = {
+  javascript: "",
+  typescript: "",
+  python: "",
+  java: "",
+  csharp: "",
+  php: "",
+  cpp: "",
+  ruby: "",
+  swift: "",
+  rust: "",
+  dart: "",
+  go: "",
+  kotlin: "",
+};
+  
+
+
